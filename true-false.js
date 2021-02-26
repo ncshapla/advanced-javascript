f1c3542ac1df06241b9df6a3c742de84c09fa34d
Shapla <shapla0777@gmail.com>
2021-02-26 11:31:49 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// const name = "Shounak"
+// falsy
+// false
+// ""
+// 0
+// undefined
+// null
+// NaN
+// truthy
+// ' ','0', []
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
