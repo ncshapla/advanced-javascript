f4f9e5e1dc832229c5dbc1cdef5a6ae542bfadcf
Shapla <shapla0777@gmail.com>
2021-02-26 18:36:43 +0600

    null-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num);
+    return
+}
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smart dude", phone:9983484};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let age = [2, 4, 5];
+console.log([11]);
